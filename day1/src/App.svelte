6dc774cbcc4c5d42c5ad3b72bfff995f3a67e3d2
KomaCrazy <96609349+KomaCrazy@users.noreply.github.com>
2022-05-10 14:59:18 +0700

    1
    1

---
+++ day1/src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { onMount } from "svelte";
+	import { apiData, drinkNames } from './store.js';
+	
+	onMount(async () => {
+	  fetch("http://192.168.1.50:5000/2")
+	  .then(response => response.json())
+	  .then(data => {
+			console.log(data);
+		apiData.set(data);
+	  }).catch(error => {
+		console.log(error);
+		return [];
+	  });
+	});
+	</script>
+	
+	<main>
+		<h1>Whiskey Drinks Menu</h1>
+		<ul>
+		{#each $drinkNames as drinkName}
+			<li>{drinkName}</li>
+		{/each}
+		</ul>
+	</main>
+	
+	<style>
+	
+	</style>
